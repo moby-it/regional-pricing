075cde484bba2efcc092df8aaabbbad457b817a9
Stefanos Palaiochoritis <uranus.p@gmail.com>
2024-01-12 16:56:25 +0200

    Created a get endpoint to the express app which takes the latitude and longitude from the query string, checks if they are truthy and if yes then makes a call to the open-street-map nominatim api from which then extracts and sends the Country name back to the client.

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.listen(process.env.PORT, () => {
     console.log(`App listening to port ${process.env.PORT}`)
 });
 
-app.get('/location', (req, res) => {
-    res.send('works');
-})
+app.get('/location', async (req, res) => {
+    const { lat, lon } = req.query;
+    if (!lat || !lon) {
+        return res.send('No coordinates were provided!');
+    }
+    const country = await axios.get(`https://nominatim.openstreetmap.org/reverse.php?lat=${lat}&lon=${lon}&zoom=3&format=jsonv2&accept-language=en`).then(data => data.data.address.country);
+    return res.send(country);
+});
