9698dac93194a6f86d8102023b61df3df426563b
GrigorisPs <uranus.p@gmail.com>
2024-01-24 15:57:18 +0200

    moved location endpoint to routes folder. changed seedDatabase function to drop the table everytime the app restarts.

--- index.mjs
+++ index.mjs
@@ -6,43 +6,14 @@ import { fetchByLatLon } from './country/getByLatLon.mjs';
 import { seedDatabase } from './db/seedDatabase.mjs';
 import * as v from 'valibot';
 import initPriceRouter from './routes/initial_price.route.mjs';
+import locationRouter from './routes/location.route.mjs';
 configDotenv();
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 await seedDatabase();
 app.use('/initialPrices', initPriceRouter);
-
-
-app.get('/location', async (req, res) => {
-    try {
-        const { lat, lon } = req.query;
-        if (!lat || !lon) {
-            return res.status(400).send('No coordinates were provided!');
-        }
-        // @ts-ignore
-        const country = await fetchByLatLon(lat, lon);
-        return res.send(country);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error);
-    }
-});
-
-app.get('/countryByIp', async (req, res) => {
-    try {
-        const ipHeader = req.headers['x-forwarded-for'];
-        const ip = v.parse(v.string(), ipHeader);
-        if (!ip) {
-            return res.status(400).send({ message: "ip missing from headers" });
-        }
-        const country = await fetchByIp(ip);
-        return res.send(country);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error);
-    }
-});
+app.use('/country', locationRouter)
 
 app.listen(port, () => {
     console.log(`App listening to port ${port}`);
---
+++ routes/location.route.mjs
@@ -0,0 +1,35 @@
+import express from "express";
+import { fetchByIp } from "../country/getByIp.mjs";
+import * as v from 'valibot';
+const locationRouter = express.Router();
+
+locationRouter.get('/byCoordinates', async (req, res) => {
+    try {
+        const { lat, lon } = req.query;
+        if (!lat || !lon) {
+            return res.status(400).send('No coordinates were provided!');
+        }
+        // @ts-ignore
+        const country = await fetchByLatLon(lat, lon);
+        return res.send(country);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+});
+locationRouter.get('/byIp', async (req, res) => {
+    try {
+        const ipHeader = req.headers['x-forwarded-for'];
+        const ip = v.parse(v.string(), ipHeader);
+        if (!ip) {
+            return res.status(400).send({ message: "ip missing from headers" });
+        }
+        const country = await fetchByIp(ip);
+        return res.send(country);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+});
+
+export default locationRouter;
