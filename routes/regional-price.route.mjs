79cb5e18b1cdd914a38698435b3b0b1679bc68eb
Stefanos Palaiochoritis <uranus.p@gmail.com>
2024-01-31 13:54:25 +0200

    feat. Isolated endpoint functionality
    Isolated endpoint functionality so that it can be subjected to unit testing.

--- routes/regional-price.route.mjs
+++ routes/regional-price.route.mjs
@@ -5,8 +5,8 @@ import { logger } from "../logger/logger.mjs";
 import { priceWeights } from "../price-weights/price-weights.mjs";
 import { defaultPrices } from "../default-prices/default-prices.mjs";
 const regionalPriceRouter = express.Router();
+
 regionalPriceRouter.get('/', async (req, res) => {
-    let appliedPriceWeigth = 1;
     try {
         const ipHeader = req.headers['ip-origin'];
         const { success, output: ip } = v.safeParse(v.string([v.minLength(9)]), ipHeader);
@@ -14,21 +14,11 @@ regionalPriceRouter.get('/', async (req, res) => {
             return res.status(400).send({ message: "incorrect ip provided" });
         }
         const country = await fetchByIp(ip);
-        for (const priceWeight of priceWeights) {
-            if (country === priceWeight.Country) {
-                appliedPriceWeigth = priceWeight.priceWeight;
-            }
-        }
-        if (appliedPriceWeigth === 1) {
+        const priceWeight = checkForPriceWeight(country)
+        if (priceWeight === 1) {
             return res.send({ country, defaultPrices });
         }
-        const regionalPrices = [];
-        for (const defaultPrice of defaultPrices) {
-            regionalPrices.push({
-                ...defaultPrice,
-                Cost: defaultPrice.Cost * appliedPriceWeigth
-            })
-        }
+        const regionalPrices = getRegionalPrices(priceWeight);
         return res.send({ country, defaultPrices, regionalPrices });
     } catch (error) {
         logger.error(error);
@@ -36,3 +26,23 @@ regionalPriceRouter.get('/', async (req, res) => {
     }
 });
 export default regionalPriceRouter;
+
+function checkForPriceWeight(country) {
+    let appliedPriceWeigth = 1;
+    for (const priceWeight of priceWeights) {
+        if (country === priceWeight.Country) {
+            appliedPriceWeigth = priceWeight.priceWeight;
+        }
+    }
+    return appliedPriceWeigth;
+}
+
+function getRegionalPrices(priceWeight) {
+    const regionalPrices = [];
+    for (const defaultPrice of defaultPrices) {
+        regionalPrices.push({
+            ...defaultPrice,
+            Cost: defaultPrice.Cost * priceWeight
+        })
+    }
+}
